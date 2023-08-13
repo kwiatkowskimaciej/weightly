694067cf2a6bc46c1af50ba92ecd6bbe354a68a6
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-08-13 19:32:56 +0200

    feat(week-calendar): add responsive week-calendar to the home page

--- app/TopBar.tsx
+++ app/TopBar.tsx
@@ -1,22 +1,32 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+import { MonthCalendar } from '@/components/MonthCalendar';
 
 export default function TopBar() {
   const pathname = usePathname().slice(1);
+  let [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex items-center justify-between w-full h-16 px-4 text-4xl justi font-header text-stone-50 bg-stone-900 sm:pl-24 xl:pl-[376px]">
+    <div className="flex items-center justify-between w-full h-16 px-4 text-4xl justi font-header text-stone-50 bg-stone-900 sm:pl-24 xl:pl-[376px] sm:pt-3 sm:mb-3">
       <span>{pathname}</span>
       <div className="flex gap-6">
         <span key={'search'} className="material-symbols-outlined">
           search
         </span>
-        <span key={'calendar_month'} className="material-symbols-outlined">
+        <button
+          key={'calendar_month'}
+          className="material-symbols-outlined"
+          onClick={() => setIsOpen(true)}
+        >
           calendar_month
-        </span>
-        {pathname === 'profile' && <span className="material-symbols-outlined">settings</span>}
+        </button>
+        {pathname === 'profile' && (
+          <span className="material-symbols-outlined">settings</span>
+        )}
       </div>
+      <MonthCalendar isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
   );
 }
