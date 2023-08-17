7cab87d4375f8dccdfa2bbd510d4f2f78502ade8
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-08-17 20:23:50 +0200

    feat(user-auth): Add user profile available after log in

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,14 @@
 import WorkoutCard from '@/components/WorkoutCard/WorkoutCard';
 import { WeekCalendar } from './WeekCalendar';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
   return (
     <>
       <WeekCalendar />
